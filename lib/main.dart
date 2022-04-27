1df5b223a8f3d8f1aa07ebea4349d56324442b24
Samvit <jain.samvit@gmail.com>
2022-04-28 00:02:06 +0530

    Minor refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // primarySwatch: Colors.blue,
         appBarTheme: AppBarTheme(
-          color: Color.fromARGB(255, 247, 250, 247),
+          color: const Color.fromARGB(255, 247, 250, 247),
           elevation: 0,
           titleTextStyle: GoogleFonts.playfairDisplay().copyWith(
             color: Colors.black,
@@ -29,53 +28,6 @@ class MyApp extends StatelessWidget {
         ),
       ),
       home: const CountryPage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
